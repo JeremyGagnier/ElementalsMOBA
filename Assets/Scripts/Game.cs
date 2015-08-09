5b32f2b67ee5adf48379735346b4dcb4e0daed76
Jeremy Gagnier <jer.gagnier@gmail.com>
2015-08-09 19:09:19 -0400

    Reorganized some files and made a game class to handle things like physics and the world

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour
+{
+    public GameObject physicsPrefab;
+    public GameObject worldPrefab;
+    public GameObject fireElemental;
+
+    private PhysicsManager physics = null;
+    private World world = null;
+
+	void Start ()
+    {
+        // Create the fundamental classes and set them up
+        physics = Instantiate(physicsPrefab).GetComponent<PhysicsManager>();
+        world = Instantiate(worldPrefab).GetComponent<World>();
+        physics.world = world;
+
+        CreatePlayer(fireElemental);
+    }
+
+    void CreatePlayer(GameObject elemental)
+    {
+        GameObject player = Instantiate(elemental);
+        world.localPlayer = player;
+        physics.AddMover(player.GetComponent<Player>());
+    }
+
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public struct Sphere
+{
+    int cx;
+    int cy;
+    FInt x;
+    FInt y;
+    FInt r;
+};
+
+
+public class Move
+{
+    public delegate bool trigger();
+    public MoveState[] move = null;
+
+
+    public Move()
+    {
+
+    }
+}
+
+public class MoveState
+{
+
+    int startFrame;
+    int frameLength;
+
+    public MoveState(int start, int duration)
+    {
+        startFrame = start;
+        frameLength = duration;
+    }
+
+    /*
+    public Sphere[] GetHitSpheres()
+    {
+        return null;
+    }*/
+
+
+}
